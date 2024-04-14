486bff9aeb50f953697d371265dcfab25f2bdca2
Sawera Tahir <141661220+SaweraTahir3@users.noreply.github.com>
2024-04-14 10:54:33 +0500

    Update app.js

--- SIGNIN & login/app.js
+++ SIGNIN & login/app.js
@@ -1,46 +1,45 @@
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDNBEVM8GKRS8PfRj6sZpSRe2N7u9HU5xE",
-//     authDomain: "fir-project-1-f42d4.firebaseapp.com",
-//     databaseURL: "https://fir-project-1-f42d4-default-rtdb.firebaseio.com",
-//     projectId: "fir-project-1-f42d4",
-//     storageBucket: "fir-project-1-f42d4.appspot.com",
-//     messagingSenderId: "700157840866",
-//     appId: "1:700157840866:web:99a49d9d58555357777dbb"
-//   };
+ const firebaseConfig = {
+    apiKey: "AIzaSyDNBEVM8GKRS8PfRj6sZpSRe2N7u9HU5xE",
+    authDomain: "fir-project-1-f42d4.firebaseapp.com",
+   databaseURL: "https://fir-project-1-f42d4-default-rtdb.firebaseio.com",
+    projectId: "fir-project-1-f42d4",
+    storageBucket: "fir-project-1-f42d4.appspot.com",
+    messagingSenderId: "700157840866",
+    appId: "1:700157840866:web:99a49d9d58555357777dbb"
+   };
 
 
-// const frb = firebase.initializeApp(firebaseConfig);
-// console.log(frb.auth)
-// console.log(frb.database)
+ const frb = firebase.initializeApp(firebaseConfig);
+ console.log(frb.auth)
+ console.log(frb.database)
 
 
-// function send(){
-//     let name = document.getElementById("name");
-//     let lastname = document.getElementById("lastname")
-//     let email = document.getElementById("email")
-//     let password = document.getElementById("password")
-//     let password2 = document.getElementById("RepeatPassword") 
-//     console.log(name.value, lastname.value,email.value, password.value, password2.value);
-//     document.getElementsByClassName(".btn1").onclick = function(){
-//         window.location.href = "./login.html";
-//         console.log(btn1)
-//     }
+ function send(){
+    let name = document.getElementById("name");
+    let lastname = document.getElementById("lastname")
+    let email = document.getElementById("email")
+    let password = document.getElementById("password")
+   let password2 = document.getElementById("RepeatPassword") 
+    console.log(name.value, lastname.value,email.value, password.value, password2.value);
+    document.getElementsByClassName(".btn1").onclick = function(){        window.location.href = "./login.html";
+        console.log(btn1)
+     }
 
         
-//         let obj ={
-//             name:name.value,
-//             lastname:lastname.value,
-//             email:email.value,
-//             password:password.value,
-//             password2:password2.value,
+        let obj ={
+            name:name.value,
+            lastname:lastname.value,
+            email:email.value,
+             password:password.value,
+            password2:password2.value,
         
-//         }
+        }
 
-//         console.log(obj)
+        console.log(obj)
 
-//     frb.database().ref("student").push(obj)
+    frb.database().ref("student").push(obj)
     
-// }
+ }
 
 
 function send(){
@@ -59,24 +58,23 @@ function send(){
     
 
 
-// (function(){
-//     const feedback = document.querySelector("#feedback")
-//     feedback.addEventListener('submit' ,function(e) {
-//  e.preventDefault()
-//  const input3 = document.querySelector('.input3');
-//  const input4 = document.querySelector('.input4 ');
-//  const  messageContent = document.querySelector('#profile');
+ (function(){
+    const feedback = document.querySelector("#feedback")
+    feedback.addEventListener('submit' ,function(e) {
+  e.preventDefault()
+ const input3 = document.querySelector('.input3');
+ const input4 = document.querySelector('.input4 ');
+ const  messageContent = document.querySelector('#profile');
     
-//  if(input3.value === ''){
-//     feedback.classList.add("show")
-//     setTimeout(function(){
-//         feedback.classList.remove('show')
-//         console.log(setTimeout)
-//     } , 1000)
-//  }
-//     else{
-//         messageContent.textContent = input3.value
-//         input3.value = '';
-//  }
-// }})();
-// // 
+  if(input3.value === ''){
+    feedback.classList.add("show")
+    setTimeout(function(){
+        feedback.classList.remove('show')
+        console.log(setTimeout)
+    } , 1000)
+  }
+     else{
+         messageContent.textContent = input3.value
+        input3.value = '';
+ }
+ }})();
